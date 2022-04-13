04bd9f885657d0f45e9acf96251e3eaa204467f9
SuperCoolCucumber <87070493+SuperCoolCucumber@users.noreply.github.com>
2022-04-13 15:25:56 +0300

    Add files via upload

---
+++ Class1.R
@@ -0,0 +1,28 @@
+log(8)
+log(sqrt(4)) #bad R bitch
+log (x=8, base=2)
+
+shit <- 10
+class(shit)
+
+
+shit <- 10L
+is.integer(shit)
+
+true <- TRUE
+ture == TRUE
+true == TRUE
+!true
+!!true
+
+c(4, 8, 15, 16, 23, 42)
+1:100
+seq(1, 10, length.out=2)
+rep(1, 5)
+rep(1:3, 3:1)
+rep(1:3, 3)
+sum(1:1000)
+
+c(2, 3, 4000)
+c(1:20)
+c(20:1)
